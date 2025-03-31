d5367582278a9396f285da244b37450056e38c0d
cwright3bsu <cwright3@bsu.edu>
2025-03-31 16:17:51 -0400

    Create main.jsx
    ➤ Add main.jsx inside src/

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CustomsSimulation from './CustomsSimulation'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CustomsSimulation />
+  </React.StrictMode>,
+)
